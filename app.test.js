6e31296534fe475a7e3efad488ca6f8f81704c3f
ThanhDong00 <thanhdonganmy@gmail.com>
2025-06-20 14:13:07 +0700

    test: add unit test case

--- app.test.js
+++ app.test.js
@@ -1,7 +1,7 @@
 const { add, subtract } = require("./app");
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(add(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(2);
 });
 
 test("subtracts 5 - 2 to equal 3", () => {
@@ -16,4 +16,8 @@ test("adds negative numbers correctly", () => {
 test("subtracts negative numbers correctly", () => {
   expect(subtract(2, 5)).toBe(-3);
   expect(subtract(0, 0)).toBe(0);
+});
+
+test("adds 2 + 2 to equal 4", () => {
+  expect(add(2, 2)).toBe(3);
 });
